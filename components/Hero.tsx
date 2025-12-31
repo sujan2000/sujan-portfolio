b2a0937aeacc51e15bfbde7ef9a08dd211ce87c8
sujan2000 <meena.sujan14@gmail.com>
2025-12-31 14:29:11 +0530

    work section implemnetation

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,47 +1,76 @@
-import React from 'react'
-import { Spotlight } from './ui/Spotlight'
-import { TextGenerateEffect } from './ui/TextGenerateEffect'
-import MagicButton from './ui/MagicButton'
-import { FaLocationArrow } from 'react-icons/fa6'
+import { FaLocationArrow } from "react-icons/fa6";
+
+import MagicButton from "./ui/MagicButton";
+import { Spotlight } from "./ui/Spotlight";
+import { TextGenerateEffect } from "./ui/TextGenerateEffect";
+
 const Hero = () => {
     return (
-        <div className='pb-20 pt-36'>
+        <div className="pb-20 pt-36">
+            {/**
+       *  UI: Spotlights
+       *  Link: https://ui.aceternity.com/components/spotlight
+       */}
             <div>
-                <Spotlight className='-top-40 -left-10 md:-left-32 md:-top-20 h-screen' fill='white' />
-                <Spotlight className='top-10 left-full h-[80vh] w-[50vw]' fill='purple' />
-                <Spotlight className='top-28 left-80 h-[80vh] w-[50vw]' fill='blue' />
+                <Spotlight
+                    className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
+                    fill="white"
+                />
+                <Spotlight
+                    className="h-[80vh] w-[50vw] top-10 left-full"
+                    fill="purple"
+                />
+                <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="blue" />
             </div>
-            <div className="relative left-0 top-0 flex h-screen w-full items-center justify-center bg-white/3 dark:bg-black/20">
-                <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white
-                 mask-[radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black" />
 
-                <div className="flex justify-center relative my-20 z-10">
-                    <div className='max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] text-center flex flex-col justify-center'>
-                        <h2 className='uppercase tracking-widest text-xs text-center text-blue-100 max-w-80'>
-                            Dynamic Web Magic with Next.js
-                        </h2>
+            {/**
+       *  UI: grid
+       *  change bg color to bg-black-100 and reduce grid color from
+       *  0.2 to 0.03
+       */}
+            <div
+                className="h-screen w-full dark:bg-black-100 dark:bg-grid-white/[0.03] bg-grid-black-100/[0.2]
+       absolute top-0 left-0 flex items-center justify-center"
+            >
+                {/* Radial gradient for the container to give a faded look */}
+                <div
+                    // chnage the bg to bg-black-100, so it matches the bg color and will blend in
+                    className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100
+          [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"
+                />
+            </div>
 
-                        <TextGenerateEffect
-                            words="Crafting Interactive Experiences with Next.js"
-                            className="mt-4 text-4xl md:text-6xl lg:text-7xl font-extrabold leading-tight"
-                            filter={true}
-                            duration={0.7}
+            <div className="flex justify-center relative my-20 z-10">
+                <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
+                    <p className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
+                        Dynamic Web Magic with Next.js
+                    </p>
+
+                    {/**
+           *  Link: https://ui.aceternity.com/components/text-generate-effect
+           *
+           *  change md:text-6xl, add more responsive code
+           */}
+                    <TextGenerateEffect
+                        words="Creating Stunning web applications | Sujan Dev"
+                        className="text-center text-[40px] md:text-5xl lg:text-6xl"
+                    />
+
+                    <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
+                        Hi! I&apos;m Sujan, a Full-Stack Web Developer based in India.
+                    </p>
+
+                    <a href="#about">
+                        <MagicButton
+                            title="Show my work"
+                            icon={<FaLocationArrow />}
+                            position="right"
                         />
-                        <p className='mt-3 text-center text-black/70 dark:text-white/70 md:text-lg leading-relaxed tracking-wide'>
-                            Hi, I&apos;m SUJAN, a passionate web developer building dynamic and responsive web applications using Next.js.
-                        </p>
-                        <a href="#about">
-                            <MagicButton
-                                title="Show my work"
-                                icon={<FaLocationArrow />}
-                                position="right"
-                            />
-                        </a>
-                    </div>
+                    </a>
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Hero
+export default Hero;
--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -90,8 +90,8 @@ export const BentoGridItem = ({
                 </div>
                 {id === 6 && (
                     <BackgroundGradientAnimation>
-                        <div className="absolute z-50 flex items-center
-                         justify-center text-white font-bold"/>
+                        {/* <div className="absolute z-50 flex items-center
+                         justify-center text-white font-bold"/> */}
                     </BackgroundGradientAnimation>
                 )}
 
