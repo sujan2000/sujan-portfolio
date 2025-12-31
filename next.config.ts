0027075ba6d8e18e6940f3171ca39836bede6bd4
sujan2000 <meena.sujan14@gmail.com>
2026-01-01 01:16:24 +0530

    output export add

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  output: 'export'
 };
 
 export default withSentryConfig(nextConfig, {
